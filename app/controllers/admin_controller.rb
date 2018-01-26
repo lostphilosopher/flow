8966beb1bb97162b7b58dbb2d088fd634de07f08
Wyatt Andersen <wandersen02@gmail.com>
2018-01-26 14:51:28 -0600

    Minor bug fixes and improvments

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,7 +1,40 @@
 class AdminController < ApplicationController
   before_action :validate_admin_or_creator, only: [:index]
-  before_action :validate_admin, only: [:users]
+  before_action :validate_admin, except: [:index]
+  before_action :find_user, only: [:make_admin, :make_creator, :make_user, :delete_user]
 
   def index
+  end
+
+  # Users come from Devise hack availability here
+  # @todo This, but better
+  def users
+    @users = User.all
+  end
+
+  def make_admin
+    @user.update(role: 'admin')
+    redirect_to admin_users_path
+  end
+
+  def make_creator
+    @user.update(role: 'creator')
+    redirect_to admin_users_path
+  end
+
+  def make_user
+    @user.update(role: nil)
+    redirect_to admin_users_path
+  end
+
+  def delete_user
+    @user.destroy
+    redirect_to admin_users_path
+  end
+
+  private
+
+  def find_user
+    @user = User.find_by(id: params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,10 @@ Rails.application.routes.draw do
   resources :stages
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'admin', to: 'admin#index'
+  get 'admin/users', to: 'admin#users'
+  post 'admin/users/make_admin', to: 'admin#make_admin'
+  post 'admin/users/make_creator', to: 'admin#make_creator'
+  post 'admin/users/make_user', to: 'admin#make_user'
+  delete 'admin/users/delete_user', to: 'admin#delete_user'
   root to: 'home#index'
 end
