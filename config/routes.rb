ab03ddb37efbc3d18a8641cc4cb1756fa0a21b21
Wyatt Andersen <wandersen02@gmail.com>
2018-01-26 15:30:24 -0600

    Bugfixes 011618 (#35)
    * Minor bug fixes and improvments
    
    * Adding tutorial first draft

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,13 @@ Rails.application.routes.draw do
   resources :stages
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'admin', to: 'admin#index'
+  get 'admin/users', to: 'admin#users'
+  get 'admin/tutorial', to: 'admin#tutorial'
+  post 'admin/users/make_admin', to: 'admin#make_admin'
+  post 'admin/users/make_creator', to: 'admin#make_creator'
+  post 'admin/users/make_user', to: 'admin#make_user'
+  delete 'admin/users/delete_user', to: 'admin#delete_user'
+
+
   root to: 'home#index'
 end
