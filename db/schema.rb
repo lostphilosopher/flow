c9b0e6d69b05105835a6f9f75f259e4ff8003ed7
Wyatt Andersen <wandersen02@gmail.com>
2018-01-28 17:37:35 -0600

    Adding migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180128223036) do
+ActiveRecord::Schema.define(version: 20180128231257) do
 
   create_table "cms", force: :cascade do |t|
     t.string   "name"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 20180128223036) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.text     "image_meta"
   end
 
   create_table "users", force: :cascade do |t|
